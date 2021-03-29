1755feb69a6ce92ed31844e213385763cd7836e3
EH30 <games.eh2000@gmail.com>
2021-03-29 13:58:36 +0530

    Add files via upload

--- tictactoe.py
+++ tictactoe.py
@@ -1,103 +1,39 @@
-def check_corners():
-    global board
+def check_corners(board):
 
     if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != " ":
-        matched = True
-        for x in range(3):
-            if board[x][x] == " ":
-                matched = False
-                break
-        
-        if matched:
-            return True
+        return True
     elif board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[0][2] != " ":
-        count = 0
-        matched = True
-        for x in range(2, -1, -1):
-            if board[count][x] == " ":
-                matched = False
-                break
-            
-            count += 1
-        
-        if matched:
-            return True
+        return True
     else:
         return False
 
 
-def check_vertical():
-    global board
+def check_vertical(board):
 
     if board[0][0] == board[1][0] and board[1][0] == board[2][0] and board[0][0] != " ":
-        matched = True
-        for x in range(3):
-            if board[x][0] == " ":
-                matched = False
-                break
-        
-        if matched:
-            return True
-        
+        return True
     elif board[0][1] == board[1][1] and board[1][1] == board[2][1] and board[0][1] != " ":
-        matched = True
-        for x in range(3):
-            if board[x][1] == " ":
-                matched = False
-        
-        if matched:
-            return True
+        return True
     elif board[0][2] == board[1][2] and board[1][2] == board[2][2] and board[0][2] != " ":
-        matched = True
-        for x in range(3):
-            if board[x][2] == " ":
-                matched = False
-                break
-        
-        if matched:
-            return True
+        return True
     else:
         return False
 
 
-def check_horizontal():
-    global board
+def check_horizontal(board):
 
     if board[0][0] == board[0][1] and board[0][1] == board[0][2] and board[0][0] != " ":
-        matched = True
-        for x in range(3):
-            if board[0][x] == " ":
-                matched = False
-                break
-        
-        if matched:
-            return True
+        return True
     
     elif board[1][0] == board[1][1] and board[1][1] == board[1][2] and board[1][0] != " ":
-        matched = True
-        for x in range(3):
-            if board[1][x] == " ":
-                matched = False
-                break
-        
-        if matched:
-            return True
+        return True
 
     elif board[2][0] == board[2][1] and board[2][1] == board[2][2] and board[2][0]!= " ":
-        matched = True
-        for x in range(3):
-            if board[2][x] == " ":
-                matched = False
-                break
-        
-        if matched:
-            return True
+        return True
     else:
         return False
 
-def check_full():
-    global board
-
+def check_full(board):
     for x in range(3):
         for i in range(3):
             if board[x][i] == " ":
@@ -106,8 +42,7 @@ def check_full():
     return True
 
 
-def display():
-    global board
+def display(board):
 
     display_board = """ 
       {0}  | {1}  | {2}  
@@ -128,8 +63,6 @@ if __name__ == "__main__":
     }
 
     board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
-
-    breaker = False
     turn = 0
 
     print("moves: a1,a2,a3, b1,b2,b3, c1,c2,c3")
@@ -156,16 +89,16 @@ if __name__ == "__main__":
         except KeyError:
             print("That's not a valid option")
 
-        print(display())
+        print(display(board))
         
-        if check_horizontal():
+        if check_horizontal(board):
             if turn == 1:
                 print("Winner: Player 1")
                 break
             else:
                 print("Winner: Player 2")
                 break
-        elif check_vertical():
+        elif check_vertical(board):
             if turn == 1:
                 print("Winner: Player 1")
                 break
@@ -173,7 +106,7 @@ if __name__ == "__main__":
                 print("Winner: Player 2")
                 break
         
-        elif check_corners():
+        elif check_corners(board):
             if turn == 1:
                 print("Winner: Player 1")
                 break
@@ -182,6 +115,6 @@ if __name__ == "__main__":
                 break
         
 
-        if check_full():
+        if check_full(board):
             print("Game ended in a draw.")
             break
